685d38d94d2855809f79981ba1dd6ba028f003d0
root <root@OzLap>
2024-02-19 23:13:01 -0500

    secuential matrix multiplication added

---
+++ matrix multiplication/secuential/run_tests.sh
@@ -0,0 +1,30 @@
+#!/bin/bash
+
+# Name of the executable
+EXECUTABLE="./matrix_multiplication_sequential.bin"
+
+# Output file for CSV results
+OUTPUT_FILE="sequential_results.csv"
+
+# Initialize the results file and write the header
+echo "N, CPU_Time" > $OUTPUT_FILE
+
+# Initial value of N
+N=200
+
+# Number of loops (10 iterations to double N each time)
+LOOPS=0
+
+# Scaling factor for N in each iteration
+FACTOR=2
+
+# Execute the program for different values of N and collect the results
+while [ $LOOPS -lt 10 ]
+do
+    echo "Running for N = $N"
+    $EXECUTABLE $N >> $OUTPUT_FILE
+    N=$((N * FACTOR))
+    LOOPS=$((LOOPS + 1))
+done
+
+echo "Execution completed. Results saved in $OUTPUT_FILE"
