5f06e7be66e69423bd9507e2db5ed067643c76e2
root <root@DESKTOP-J8QN23Q>
2024-03-06 12:24:33 -0500

    cambio en el test todo completo

--- matrix multiplication/processPipes/run_test.sh
+++ matrix multiplication/processPipes/run_test.sh
@@ -15,7 +15,6 @@ OUTPUT_FILE="processpipes_results.csv"
 #FACTOR=2
 
 # Number of processes to use
-NUM_PROCESSES=4
 
 # Execute the program for different values of N and collect the results
 #while [ $LOOPS -lt 10 ]
@@ -30,13 +29,18 @@ NUM_PROCESSES=4
 #    LOOPS=$((LOOPS + 1))
 #done
 
-for N in 200 500 1000 2000 3000 4000 5000 6000 7000 8000
+for NUM_PROCESSES in 8 4 2
 do
-     echo "Running for N = $N with $NUM_PROCESSES parallel processes"
+ for N in 200 500 1000 2000 3000 4000 5000 6000 7000 8000
+ do
     
+     echo "Running for N = $N with $NUM_PROCESSES parallel processes" >> $OUTPUT_FILE
+
     # Run the executable with the specified number of processes and wait for it to finish
+    
     $EXECUTABLE $N $NUM_PROCESSES >> $OUTPUT_FILE 
- 
+
+ done
 done
 
 echo "Execution completed. Results saved in $OUTPUT_FILE"
