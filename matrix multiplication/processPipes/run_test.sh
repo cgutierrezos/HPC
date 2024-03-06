b739882544c56156fb193b1e655c2a85f7f71c1d
root <root@DESKTOP-J8QN23Q>
2024-03-06 11:38:48 -0500

    pipes with limit process - configure process

--- matrix multiplication/processPipes/run_test.sh
+++ matrix multiplication/processPipes/run_test.sh
@@ -7,28 +7,36 @@ EXECUTABLE="./matrix_multiplication_process_pipes.bin"
 OUTPUT_FILE="processpipes_results.csv"
 
 # Initial value of N
-N=100
 
 # Number of loops (10 iterations to double N each time)
-LOOPS=0
+#LOOPS=0
 
 # Scaling factor for N in each iteration
-FACTOR=2
+#FACTOR=2
 
 # Number of processes to use
 NUM_PROCESSES=4
 
 # Execute the program for different values of N and collect the results
-while [ $LOOPS -lt 10 ]
-do
-    echo "Running for N = $N with $NUM_PROCESSES parallel processes"
+#while [ $LOOPS -lt 10 ]
+#do
+#    echo unning for N = $N with $NUM_PROCESSES parallel processes
     
     # Run the executable with the specified number of processes and wait for it to finish
-    $EXECUTABLE $N $NUM_PROCESSES >> $OUTPUT_FILE &
-    wait
+#    $EXECUTABLE $N $NUM_PROCESSES >> $OUTPUT_FILE &
+#    wait
     
-    N=$((N * FACTOR))
-    LOOPS=$((LOOPS + 1))
+#    N=$((N * FACTOR))
+#    LOOPS=$((LOOPS + 1))
+#done
+
+for N in 200 500 1000 2000 3000 4000 5000 6000 7000 8000
+do
+     echo "Running for N = $N with $NUM_PROCESSES parallel processes"
+    
+    # Run the executable with the specified number of processes and wait for it to finish
+    $EXECUTABLE $N $NUM_PROCESSES >> $OUTPUT_FILE 
+ 
 done
 
 echo "Execution completed. Results saved in $OUTPUT_FILE"
