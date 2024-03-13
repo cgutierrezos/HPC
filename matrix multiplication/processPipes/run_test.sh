2cb7587c0b3d52a1a3f28c853ac65d8cb189ca45
unknown <cgutierrezos@utp.edu.co>
2024-03-12 19:31:18 -0500

    Parallel Process with Pipes, Test updated

--- matrix multiplication/processPipes/run_test.sh
+++ matrix multiplication/processPipes/run_test.sh
@@ -6,43 +6,22 @@ EXECUTABLE="./matrix_multiplication_process_pipes.bin"
 # Output file for CSV results
 OUTPUT_FILE="processpipes_results.csv"
 
-# Initial value of N
-
-# Number of loops (10 iterations to double N each time)
-#LOOPS=0
-
-# Scaling factor for N in each iteration
-#FACTOR=2
-
-# Number of processes to use
-
-# Execute the program for different values of N and collect the results
-#while [ $LOOPS -lt 10 ]
-#do
-#    echo unning for N = $N with $NUM_PROCESSES parallel processes
-    
-    # Run the executable with the specified number of processes and wait for it to finish
-#    $EXECUTABLE $N $NUM_PROCESSES >> $OUTPUT_FILE &
-#    wait
-    
-#    N=$((N * FACTOR))
-#    LOOPS=$((LOOPS + 1))
-#done
 
 echo "Processes, N, CPU_Time" > $OUTPUT_FILE
 
-for NUM_PROCESSES in 16 12 8 4 2
+# Execute the program for different values of N and NUM_PROCESS, then collect the results
+for NUM_PROCESSES in 2 4 6 8 12 16
 do
- for N in 200 500 1000 2000 3000 4000 5000 6000 7000 8000
- do
-    
-     echo "Running for N = $N with $NUM_PROCESSES parallel processes"
+    for N in 200 500 1000 2000 3000 4000 5000 6000 7000 8000
+    do
+        
+        echo "Running for N = $N with $NUM_PROCESSES parallel processes"
 
-    # Run the executable with the specified number of processes and wait for it to finish
-    
-    $EXECUTABLE $N $NUM_PROCESSES >> $OUTPUT_FILE 
+        # Run the executable with the specified number of processes and wait for it to finish
+        
+        $EXECUTABLE $N $NUM_PROCESSES >> $OUTPUT_FILE 
 
- done
+    done
 done
 
 echo "Execution completed. Results saved in $OUTPUT_FILE"
